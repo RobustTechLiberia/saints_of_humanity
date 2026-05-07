c715f89734717a18b711f03980d708a9308215c1
Gabriel_31997 <gabrielwkun@gmail.com>
2026-05-07 17:04:52 +0000

    deployment issue still undergoing fixing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || "/",
+  base: "/saints_of_humanity/",
 
-  plugins: [tailwindcss(), react()],
+  plugins: [react(), tailwindcss()],
 });
